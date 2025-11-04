8ae5674da20fccbe923f9fa075e9fbdd343cbf92
Nardo2424 <hartmann.bernardo@escola.pr.gov.br>
2025-11-04 17:29:55 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -46,5 +46,69 @@ const loop = setInterval(() => {
 
     //Lógica de Colisão
     //Verifica se:
-    
-})
+    //1. O cano está na zona de colisão (entre 0px e 120px da esquerda)
+    //2. O Mario está baixo demais (menos de 80px do chão)
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+        //Para todas as animações
+        pipe.style.animation = 'none';
+        pipe.style.left = '${pipePosition}px' //Congela a posição do cano
+
+        clouds.style.animation = 'none';
+        clouds.style.right = '${cloudsPosition}px'; //Congela nuvens
+
+        mario.style.animation = 'none';
+        mario.style.bottom = '${marioPosition}px'; //Congela Mario
+
+        //Troca a imagem do Mario para "game over"
+
+        mario.src = '/Images/game-over.png';
+        mario.style.width = '75px'
+        mario.style.marginLeft = '50px';
+
+        //Atualiza o estado do jogo
+        isGameOver = true;
+        restartBtn.style.display = 'block'; //Mostra botão de reiniciar
+
+        clearInterval(loop); //Para o loop principal
+    }
+
+    //Sistema de pontuação
+    //Aumenta a pontuação a cada ciclo (se o jogo não acabou)
+    if (!isGameOver) {
+        score++;
+        scoreDisplay.textContent = score; //Atualiza o display
+    }
+}, 10); //Executa a cada 10,s
+
+//Quando o botão de reiniciar for clicado
+restartBtn.addEventListener('click', () => {
+    location.reload(); //Recarrega a página inteira
+});
+
+//Adicione estas linhas temporariamente para testar:
+
+//1. Teste de posicionamento
+console.log("Posição do pipe:", pipePosition);
+console.log("Posição do Mario:", marioPosition);
+
+//2. Teste de colisão
+
+if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+    console.log("Colisão Detectada!")
+}
+
+//3. Teste de pontuação
+console.log("Pontuação atual:", score);
+
+//Dica: Adicione esta lógica dentro do loop
+if (score > 100 && score < 101) {
+    pipe.style.animationDuration = '1.5s'; //Mais rápido!
+}
+
+if (score > 300 && score < 301) {
+    pipe.style.animationDuration = '1s'; //Ainda mais rápido!
+}
+
+const jumpSound = document.getElementById('jumpSound')
+const gameOverSound = document.getElementById('gameOverSound')
+
